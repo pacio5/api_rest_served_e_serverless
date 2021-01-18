76ab66ee460e99f9b863bcd913ed8a8a5d86e8d5
Elia Pacioni <eliapacioni@gmail.com>
2021-01-18 22:45:10 +0100

    Refactoring messaggi 2
    Refactoring messaggi e messaggi d'errore.

--- api_rest_serverless_express/src/app.ts
+++ api_rest_serverless_express/src/app.ts
@@ -3,17 +3,15 @@ import express from "express";
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
-
 import { connect, disconnect } from "./database/database";
 import { UserModel } from './database/users/users.model';
-
 import Jimp from 'jimp';
 import fs from 'fs';
 import AWS from 'aws-sdk';
-
 import { v4 as uuidv4 } from 'uuid';
-
+import { authMiddleware } from "./middleware/authentication";
 import serverless from 'serverless-http';
+
 
 // Extend Express request 
 declare global {
@@ -32,7 +30,7 @@ const port = 3000;
 const secretKey = 'secretkey'
 
 
-app.get('/', (req, res) => {res.send('App is up'); });
+app.get('/', (req, res) => {res.send('App online!'); });
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -40,7 +38,7 @@ app.use(bodyParser.json())
 
 
 app.post('/signup', (req, res) => {
-  if (!req.body) res.status(400).json({ message: "empty body" })
+  if (!req.body) res.status(400).json({ message: "Dati mancanti" })
 
   let user = { _id: req.body.email, password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(5)) };
 
@@ -48,55 +46,43 @@ app.post('/signup', (req, res) => {
     try {
       const newUser = await UserModel.findOneOrCreate(user);
       if (Object.is(newUser, null)) {
-        console.log("User exists");
-        res.send("user exists");
+        res.status(403).json("Utente già esistente");
       } else {
-        console.log(`Created user ${user._id}`);
-        res.send(`Created user ${user._id}`);
+        res.status(200).json("Utente creato correttamente");
       }
     } catch (err) {
-      console.log(err);
-      res.send(`error ${err}`);
-
+      res.status(200).json(`Errore ${err}`);
     }
   })();
 
 });
 
 app.post('/login', (req, res) => {
-  if (!req.body) res.status(400).json({ message: "empty body" })
+  if (!req.body) res.status(400).json({ message: "Dati mancanti" });
   UserModel.findOne({ _id: req.body.email }, (err, userFind) => {
-    if (!userFind) res.status(401).json({ message: "incorrect email or password" });
-    if (!bcrypt.compareSync(req.body.password, userFind.password)) res.status(401).json({ message: "incorrect email or password" });
+    if (!userFind) res.status(401).json({ message: "Credenziali errate" });
+    if (!bcrypt.compareSync(req.body.password, userFind.password)) res.status(401).json({ message: "Credenziali errate" });
 
     const token = jwt.sign({ id: userFind._id }, secretKey, { expiresIn: '1h' });
-    res.json({ status: 200, message: 'user logged', token });
+    res.json({ status: 200, message: 'Accesso eseguito', token });
   });
 });
 
 // Auth middleware with external declaration
-import { authMiddleware } from "./middleware/authentication";
 app.use(authMiddleware);
-
-app.get('/secret', (req, res) => {
-  // Test route for authenticate users
-  res.status(200).json({ message: 'secret route' });
-});
 
 app.get('/profile', (req, res) => {
 
   UserModel.findOne({ _id: req.decoded.id }, (err, user) => {
-    if (!user) res.status(401).json({ status: 401, message: "no such user found" });
+    if (!user) res.status(400).json({message: "Utente non trovato" });
     res.status(200).json({ status: 200, user });
   });
 });
 
 app.delete('/profile', (req, res) => {
-  // delete user profile
-  if (!req.body) res.status(400).json({ message: 'empty body' });
-  UserModel.remove({ _id: req.body.email }, (err) => {
-    if (err) res.status(400).json({ message: 'error' });
-    res.status(200).json({ message: 'user deleted' });
+  UserModel.remove({ _id: req.decoded.id }, (err) => {
+    if (err) res.status(400).json({ message: "Errore nell\'eliminazione" });
+    res.status(200).json({ message: "Utente eliminato"  });
   })
 })
 
@@ -124,13 +110,12 @@ app.get('/charts', async (req, res) => {
       if (err) {
         throw err;
       }
-      console.log(`File uploaded successfully. ${data.Location}`);
-      // Delete file
+      // Delete temp file
       fs.unlinkSync('/tmp/' + id + '.png');
-      return res.send("Ok Elia");
+      return res.status(200).json(`Grafico salvato correttamente. ${data.Location}`);
     });
 
-  } catch (err) { throw err; }
+  } catch (err) { res.status(400).json(`Errore: ${err}`); }
 
 });
 
@@ -145,15 +130,12 @@ app.get('/list', (req, res) =>{
   const s3 = new AWS.S3();
   s3.listObjectsV2(params, function(err, data) {
     if (err) {
-      console.log(err, err.stack);
-      throw err;
-     } // an error occurred
-    else  {   console.log(data);   
-      return res.send(data.Contents);
+      res.status(400).json(`Errore: ${err}`);     
+    } 
+    else  {  
+      return res.status(200).json(data.Contents);
     }// successful response
   });
 });
-
-// app.listen(port, () => { console.log(`Server started on http://localhost:${port}`); });
 
 module.exports.handler = serverless(app);
